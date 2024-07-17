e2764e0681d330432440be174e60feea3f937b7b
thakorsarthak <143281352+thakorsarthak@users.noreply.github.com>
2024-07-17 11:35:44 +0530

    Add files via upload

---
+++ Java-Assignment/Web Technologies/src/main/java/controller/StudentController.java
@@ -0,0 +1,62 @@
+package controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.tomcat.util.security.KeyStoreUtil;
+
+import model.Student;
+
+/**
+ * Servlet implementation class StudentController
+ */
+@WebServlet("/StudentController")
+public class StudentController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public StudentController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		
+		String action = request.getParameter("action");
+		
+		if(action.equalsIgnoreCase("submit"))
+		{
+			Student s = new Student();
+			s.setFname(request.getParameter("fname"));
+			s.setLname(request.getParameter("lname"));
+			s.setEmail(request.getParameter("email"));
+			s.setContact(Long.parseLong(request.getParameter("contact")));
+			s.setGender(request.getParameter("gender"));
+			s.setPassword(request.getParameter("password"));
+			System.out.println(s);
+			
+		}
+		doGet(request, response);
+		
+	}
+
+}
---
+++ Java-Assignment/Web Technologies/src/main/java/model/Student.java
@@ -0,0 +1,54 @@
+package model;
+
+public class Student {
+	String fname,lname,address,email,gender,password;
+	Long contact;
+	public String getFname() {
+		return fname;
+	}
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+	public String getLname() {
+		return lname;
+	}
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Long getContact() {
+		return contact;
+	}
+	public void setContact(Long contact) {
+		this.contact = contact;
+	}
+	@Override
+	public String toString() {
+		return "Student [fname=" + fname + ", lname=" + lname + ", address=" + address + ", email=" + email
+				+ ", gender=" + gender + ", password=" + password + ", contact=" + contact + "]";
+	}
+	
+}
